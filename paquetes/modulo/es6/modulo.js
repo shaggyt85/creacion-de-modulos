ee5be6fd9406e31d51fdc97549478dea39237acd
Robert Medina <csoperativo@gmail.com>
2023-01-25 18:02:07 -0400

    first commit

---
+++ paquetes/modulo/es6/modulo.js
@@ -0,0 +1,15 @@
+const modulo = require("../modulo");
+
+function saludar() {
+    console.log("Hola mundo");
+}
+
+const prop1 = "Hola qwue tal";
+const prop2 = "Hola que tal";
+
+module.exports = {
+    saludar,
+    prop1,
+    prop2,
+};
+
